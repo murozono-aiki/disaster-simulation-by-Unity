7eaaa6d9a8a77a8ad025081d2aa4f672afb27175
murozono-aiki <151797695+murozono-aiki@users.noreply.github.com>
2024-12-26 11:27:22 +0900

    オブジェクト追加

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] MeshFilter buildingMeshFilter;
+    [SerializeField] MeshFilter groundMeshFilter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //buildingMeshFilter.mesh.vertices
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
